79e0ace5aebd27313eadae9bd2885c3c8eedfb8f
dev-cli234 <273791263+dev-cli234@users.noreply.github.com>
2026-05-08 03:45:49 +0800

    feat: filter rides by status and email

--- rides/views.py
+++ rides/views.py
@@ -1,8 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 
 from users.permissions import IsAdminRole
 
+from .filters import RideFilter
 from .models import Ride
 from .serializers import RideSerializer
 
@@ -10,5 +12,9 @@ from .serializers import RideSerializer
 class RideViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminRole]
-    queryset = Ride.objects.all()
     serializer_class = RideSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RideFilter
+
+    def get_queryset(self):
+        return Ride.objects.all()
