48c5fbe4340c297d11c19e6c179ef2b31c198c19
dev-cli234 <273791263+dev-cli234@users.noreply.github.com>
2026-05-08 04:08:44 +0800

    feat: support ordering rides by pickup_time asc and desc

--- rides/views.py
+++ rides/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import OrderingFilter
 
 from users.permissions import IsAdminRole
 
@@ -13,8 +14,10 @@ class RideViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminRole]
     serializer_class = RideSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = RideFilter
+    ordering_fields = ['pickup_time']
+    ordering = ['pickup_time']
 
     def get_queryset(self):
         return Ride.objects.all()
