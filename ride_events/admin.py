35f8e3b2c58edd8173ac4a5a7fd49fb44c5581ae
dev-cli234 <273791263+dev-cli234@users.noreply.github.com>
2026-05-07 23:31:21 +0800

    feat: register User, Ride, RideEvent in Django admin

--- ride_events/admin.py
+++ ride_events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RideEvent
+
+
+@admin.register(RideEvent)
+class RideEventAdmin(admin.ModelAdmin):
+    list_display = ('id_ride_event', 'id_ride', 'description', 'created_at')
+    search_fields = ('description',)
+    list_filter = ('created_at',)
--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ride
+
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = ('id_ride', 'status', 'id_rider', 'id_driver', 'pickup_time')
+    search_fields = ('id_rider__email', 'id_driver__email')
+    list_filter = ('status',)
