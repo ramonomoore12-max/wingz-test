fd186402031ba1d0b4c6d84371bd584f7f80b2d1
dev-cli234 <273791263+dev-cli234@users.noreply.github.com>
2026-05-08 22:05:09 +0800

    feat: sort rides by distance

--- rides/views.py
+++ rides/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models.expressions import RawSQL
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import OrderingFilter
@@ -8,6 +9,16 @@ from users.permissions import IsAdminRole
 from .filters import RideFilter
 from .models import Ride
 from .serializers import RideSerializer
+
+HAVERSINE_SQL = """
+    6371 * acos(
+        LEAST(1.0,
+            cos(radians(%s)) * cos(radians(pickup_latitude))
+            * cos(radians(pickup_longitude) - radians(%s))
+            + sin(radians(%s)) * sin(radians(pickup_latitude))
+        )
+    )
+"""
 
 
 class RideViewSet(viewsets.ModelViewSet):
@@ -20,4 +31,18 @@ class RideViewSet(viewsets.ModelViewSet):
     ordering = ['pickup_time']
 
     def get_queryset(self):
-        return Ride.objects.all()
+        qs = Ride.objects.all()
+
+        lat = self.request.query_params.get('lat')
+        lon = self.request.query_params.get('lon')
+
+        if lat and lon:
+            try:
+                lat, lon = float(lat), float(lon)
+                qs = qs.annotate(
+                    distance=RawSQL(HAVERSINE_SQL, (lat, lon, lat))
+                ).order_by('distance')
+            except (TypeError, ValueError):
+                pass
+
+        return qs
